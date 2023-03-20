0bf5df4eae8c84c16b203aba18b0c3203d799b10
projet-drone <snir2>
2023-03-20 13:58:49 +0000

    Version sd1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# hello world
+import time
+import sys
+from adafruit_servokit import ServoKit
+
+kit = ServoKit(channels=8)
+num1=int(sys.argv[1])
+num2=int(sys.argv[2])
+
+print(sys.argv[1])
+kit.servo[num1].angle = num2
+kit.continuous_servo[1].throttle = 1
